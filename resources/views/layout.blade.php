d9a0bf9607bc96b6cdab17dfe341eaf5d3c4e602
marcRAES <contact@marcraes.fr>
2021-12-17 12:26:40 +0100

    Add styling with Tailwind CSS

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,17 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>@yield('title', config('app.name'))</title>
-</head>
-<body>
-@yield('content')
 
-<footer>
-    <p>
-        &copy; Copyright {{ date('Y') }}
-        @if(!Route::is('app_about'))
-        &middot; <a href="{{ route('app_about') }}">About Us</a>
-        @endif
-    </p>
-</footer>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="py-6 flex flex-col justify-between items-center min-h-screen">
+    <main role="main" class="flex flex-col justify-center items-center">
+        @yield('content')
+    </main>
+
+    <footer>
+        <p class="text-gray-400">
+            &copy; Copyright {{ date('Y') }}
+            @if(!Route::is('app_about'))
+            &middot; <a href="{{ route('app_about') }}" class="text-indigo-500 hover:text-indigo-600 underline">About Us</a>
+            @endif
+        </p>
+    </footer>
 </body>
 </html>
