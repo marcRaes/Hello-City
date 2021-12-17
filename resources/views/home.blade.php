8bb38fbf82d35bf28420c7e87492421982f149e4
marcRAES <contact@marcraes.fr>
2021-12-17 11:57:14 +0100

    Make use of asset function helper to load images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-    <img src="/images/quebec_flag.png" alt="Quebec Flag" width="300px">
+    <img src="{{ asset('images/quebec_flag.png') }}" alt="Quebec Flag" width="300px">
 
     <h1>Hello from Quebec!</h1>
 
