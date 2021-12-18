1da502b8d641807cac0bb8006338951d296a6652
marcRAES <contact@marcraes.fr>
2021-12-18 12:28:47 +0100

    Addition of Route declaration possibilities

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+ * Route home
+ */
 Route::get('/', function () {
     return view('home');
 })->name('app_home');
+/*
+ * Ceci pourrait être écrit comme ceci :
+ * Route::get('/', fn() => view('home'))->name('app_home');
+ * ou comme ceci :
+ * Route::view('/', 'home')->name('app_home');
+ */
 
+/*
+ * Route About-us
+ */
 Route::get('/about-us', function () {
     return view('about');
 })->name('app_about');
+/*
+ * Ceci pourrait être écrit comme ceci :
+ * Route::get('/about-us', fn() => view('about'))->name('app_about');
+ * ou comme ceci :
+ * Route::view('/about-us', 'about')->name('app_about');
+ */
